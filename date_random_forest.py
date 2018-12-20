99cf1fc2b1ce4db46fd204cc9bb0ba9500294584
matanpel <matan.peleg@vatbox.com>
2018-12-20 19:21:24 +0200

    hood

--- date_random_forest.py
+++ date_random_forest.py
@@ -2,59 +2,76 @@
 import pandas as pd
 import numpy as np
 import string
-import re
 from fastparquet import ParquetFile
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
 
 #clean data - create corpus
+
+
+def hood(text):
+    sentence = text.split()
+    indices = (i for i,word in enumerate(sentence) if '201' in word)
+    neighbors = []
+    for ind in indices:
+        if ind < 3:
+            strt = 0
+        else:
+            strt = ind-3
+        neighbors.append(' '.join(sentence[strt:ind]) +' '+ ' '.join([sentence[ind]]) + ' ' + ' '.join(sentence[ind+1:ind+4]))
+    return neighbors
+
+
+
 def clean_data(X):
-    my_punc=list(string.punctuation)
-    my_punc.remove('-')
-    my_punc.remove('/')
-    my_punc=''.join(my_punc)
-    translator = str.maketrans('', '', my_punc)
-    
+    my_punc = list(string.punctuation)
+    #my_punc.remove('-')
+    #my_punc.remove('/')
+    my_punc = ''.join(my_punc)
+    translator = str.maketrans(my_punc, ' '*len(my_punc))
+
     corpus=[]
     for row in X.itertuples():
         text=row.Text
         if text != pd.isnull(text):
+        #if '201' in str(text):
             text = str(text).translate(translator)
+            #text = ' '.join(word for word in text.split() if len(word) < 10) # remove words longer then the word 'september'
+            #text = ' '.join(word for word in text.split() if not word.isdigit() or int(word) < 32) # remove numbers bigger than 31
+            #text = ' '.join([item for item in text.split() if item.isdigit() or not any(filter(str.isdigit, item))]) # filter words with letters & numbers --- that not good 08Aug
             #text = text.replace('\n',' ')
-            text = text.replace(' L_B ',' ')  # in Parquet '\n' == ' L_B '
-            text = text.replace(' comma ',',') # in Parquet ',' == ' comma '
-            #text=re.sub(r'\d+', '', text)  #remove numbers  ### do we want to? might hold information
-            corpus.append(text)
+            text = text.replace(' L_B ', ' ')  # in Parquet '\n' == ' L_B '
+            text = text.replace(' comma ', ',')  # in Parquet ',' == ' comma '
+            text = hood(text)
+            corpus.append(' '.join(text))
     return(corpus)
 
 #######################################################################################
 
 #read data
 
-dates = ParquetFile('/run/user/1000/gvfs/smb-share:server=nas01.local,share=rnd/data/parquet_data/rails_data.parq').to_pandas(columns=['imaginary_id','invoice_date']).dropna()
-dates['day'] = dates['invoice_date'].str.slice(0,2)
-dates['month'] = dates['invoice_date'].str.slice(3,5)
-dates['year'] = dates['invoice_date'].str.slice(6,10)
 
-dates = dates[(dates['month'].isin(['01','02','03','04','05','06'])) & (dates['year']=='2018')]
+df = ParquetFile('/run/user/1000/gvfs/smb-share:server=nas01.local,share=rnd/data/parquet_data/date_alg_results_and_ocr.parq').to_pandas()
+df = df.loc[(df['conclusion'] == 'N\A') & (df['post_match_date'] != 'N\A')]
 
-print('dates loaded')
+print('data loaded')
 
-text = ParquetFile('/run/user/1000/gvfs/smb-share:server=nas01.local,share=rnd/data/parquet_data/text_extractions_temp.parq').to_pandas()
-text.columns = ['imaginary_id', 'Text']
+df['day'] = df['post_match_date'].str.slice(0,2)
+df['month'] = df['post_match_date'].str.slice(3,5)
+df['year'] = df['post_match_date'].str.slice(6,10)
 
-print('text extractions loaded')
 
-df = pd.merge(text,dates, on='imaginary_id')
+df.loc[:, 'hood'] = clean_data(pd.DataFrame(df.Text))
+df = df[df['hood'] != '']
 
-print('data merged')
+
 print('invoices to train: ', len(df))
 
 #split data
-y = df['month'] ############################# date part
+y = df['day'] ############################# date part
 X = df[['imaginary_id','Text']]
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
 
@@ -64,20 +81,19 @@ X_train = clean_data(X_train)
 
 print('data cleaned')
 
-vectorizer = CountVectorizer(ngram_range=(1, 1), binary=True, lowercase=True, max_features=5000) ########### MODEL
+vectorizer = CountVectorizer(ngram_range=(1, 1), binary=True, lowercase=True, max_features=200) ########### MODEL
 vectorizer.fit(X_train)
+word_matrix = vectorizer.transform(X_train)
 
 print('vectorizer done')
 
-word_matrix = vectorizer.transform(X_train)
 tfidf = TfidfTransformer(norm='l2') ############# MODEL
 tfidf.fit(word_matrix)
+tfidf_matrix = tfidf.transform(word_matrix)
 
 print('tfidf done')
 
-tfidf_matrix = tfidf.transform(word_matrix)
-
-X_test=clean_data(X_test)
+X_test = clean_data(X_test)
 test_word_matrix = vectorizer.transform(X_test)
 test_tfidf_matrix = tfidf.transform(test_word_matrix)
 
@@ -88,9 +104,8 @@ clf.fit(tfidf_matrix, y_train)
 print('Random Forest done')
 
 
-## random forest:
+
 y_pred = clf.predict(test_tfidf_matrix)
-X_test, y_test, y_pred #### make DataFrame and analyze ###############################################
 
 print ('RF:')
 print ('----------------------')
@@ -98,4 +113,16 @@ print ('\tAccuracy: %1.3f' % accuracy_score(y_test, y_pred, normalize=True))
 print ('\tPrecision: %1.3f' % precision_score(y_test, y_pred, average='weighted',labels=np.unique(y_pred)))
 print ('\tRecall: %1.3f' % recall_score(y_test, y_pred, average='weighted',labels=np.unique(y_pred)))
 
+
+
+df1 = pd.DataFrame({'x_test': X_test, 'y_test': y_test, 'y_pred': y_pred})
+df1.loc[:, 'is_correct'] = (df1.y_test == df1.y_pred)
+df1.groupby(['y_test','is_correct']).size().unstack()
+
+
+"""
+df.loc[:, 'hood'] = clean_data(pd.DataFrame(df.Text))
+df2 = df[['Text', 'hood']]
+df2.to_csv('/home/matan/Documents/code/date/hood.csv')
+"""
 
